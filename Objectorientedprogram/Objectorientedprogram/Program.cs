1146c3339fc5885e2e58aeaab63d086d8ba31c61
shital bramhe <shitalbramhe@gmail.com>
2021-11-17 17:34:21 +0530

    [shital] : initial

---
+++ Objectorientedprogram/Objectorientedprogram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Objectorientedprograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Object Oriented Programs");
+        }
+    }
+}
