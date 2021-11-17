b6cea7415b85d9056e9b46e212f07d87aca6061e
shital bramhe <shitalbramhe@gmail.com>
2021-11-17 18:13:34 +0530

    [shital] : Output JSON string

---
+++ Objectorientedprogram/Objectorientedprogram/InventoryModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objectorientedprogram
+{
+    internal class InventoryModel
+    {
+        
+        public string Name { get; set; }
+        public string Weight { get; set; }
+        public string Price { get; set; }
+    }
+}
+
--- Objectorientedprogram/Objectorientedprogram/Program.cs
+++ Objectorientedprogram/Objectorientedprogram/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace Objectorientedprograms
+namespace Objectorientedprogram
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Object Oriented Programs");
+            
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nWelcome to Object Oriented Programs"); 
+                Console.WriteLine("Enter Progran number to get excuted \n1. Inventory management  \n2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        InventoryMain Data = new InventoryMain();
+                        Data.DisplayData(@"E:\RFP\ObjectOrientedPrograms\Objectorientedprogram\Objectorientedprogram\NewFolder\Inventory.json");
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
